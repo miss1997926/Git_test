fa9f7efab927ad33d05898b037bb6c0fba77e79f
malijie <malijie926@qq.com>
2022-09-22 15:27:28 +0800

    v1.3

--- src/main/java/com/atguigu/git/User.java
+++ src/main/java/com/atguigu/git/User.java
@@ -9,5 +9,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("git ...");
         System.out.println("git ...");
+        System.out.println("git ...");
     }
 }
