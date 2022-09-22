c5a0e24a536610ada1653159081e88b1a2821950
malijie <malijie926@qq.com>
2022-09-22 15:21:39 +0800

    v1.1

---
+++ src/main/java/com/atguigu/git/User.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: mlj
+ * @Date: 2022-09-22 15:16
+ * @Description:
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("git ...");
+    }
+}
